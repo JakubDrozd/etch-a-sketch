589cb91826c43d5b7dbe8e1179831415c1517714
Veanty <plmulticraftpl@gmail.com>
2022-06-09 14:51:22 +0200

    Complete etch-a-sketch project

---
+++ script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector(".container");
+
+const colorSelector = document.querySelector("#colorPicker");
+
+let color = "#ffffff";
+
+colorSelector.addEventListener("change", (e) => {
+  color = e.target.value;
+});
+
+let flag = false;
+container.addEventListener("mousedown", () => (flag = true));
+
+container.addEventListener("mouseup", () => (flag = false));
+
+const pickYourCanvas = document.querySelector(".pickYourCanvas");
+pickYourCanvas.addEventListener("click", () => {
+  const canvasSize = prompt("How big do you want your canvas to be?");
+  if (canvasSize > 100) {
+    alert("Your canvas is too big! (Max 100 x 100)");
+  } else {
+    container.innerHTML = `<div class="row">${'<div class="cell"></div>'.repeat(
+      canvasSize
+    )}</div>`.repeat(canvasSize);
+    const cells = document.querySelectorAll(".cell");
+    Array.from(cells).forEach((cell) => {
+      cell.addEventListener("mouseover", colorTile);
+    });
+  }
+});
+
+const colorTile = (e) => {
+  if (!flag) return;
+  e.target.style.backgroundColor = color;
+};
