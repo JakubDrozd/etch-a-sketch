b8b54048fc695b71c47d57af3a6ce105673de748
Veanty <plmulticraftpl@gmail.com>
2022-06-09 16:47:10 +0200

    Add new size input for more dynamic sizing

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const container = document.querySelector(".container");
 
+container.innerHTML = `<div class="row">${'<div class="cell"></div>'.repeat(
+  16
+)}</div>`.repeat(16);
+
 const colorSelector = document.querySelector("#colorPicker");
 
 let color = "black";
 
 const toggleBorder = document.querySelector(".toggleBorder");
+toggleBorder.addEventListener("click", hideBorder);
+
+function hideBorder() {
+  cell.style.border = "none";
+}
 
 colorSelector.addEventListener("change", (e) => {
   color = e.target.value;
@@ -15,25 +24,26 @@ container.addEventListener("mousedown", () => (flag = true));
 
 container.addEventListener("mouseup", () => (flag = false));
 
-const pickYourCanvas = document.querySelector(".pickYourCanvas");
-pickYourCanvas.addEventListener("click", () => {
-  const canvasSize = prompt("How big do you want your canvas to be?");
-  if (canvasSize > 100) {
-    alert("Your canvas is too big! (Max 100 x 100)");
-  } else if (!canvasSize) {
-    alert("Canceled");
-  } else {
-    container.innerHTML = `<div class="row">${'<div class="cell"></div>'.repeat(
-      canvasSize
-    )}</div>`.repeat(canvasSize);
-    const cells = document.querySelectorAll(".cell");
-    Array.from(cells).forEach((cell) => {
-      cell.addEventListener("mouseover", colorTile);
-    });
-  }
-});
+const size = document.querySelector(".size");
+size.addEventListener("input", changeSize);
+
+function changeSize(e) {
+  const canvasSize = e.target.value;
+  container.innerHTML = `<div class="row">${'<div class="cell"></div>'.repeat(
+    canvasSize
+  )}</div>`.repeat(canvasSize);
+  const cells = document.querySelectorAll(".cell");
+  Array.from(cells).forEach((cell) => {
+    cell.addEventListener("mouseover", colorTile);
+  });
+}
 
 const colorTile = (e) => {
   if (!flag) return;
   e.target.style.backgroundColor = color;
 };
+
+const cells = document.querySelectorAll(".cell");
+Array.from(cells).forEach((cell) => {
+  cell.addEventListener("mouseover", colorTile);
+});
